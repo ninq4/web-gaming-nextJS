737aebe7703737edfb9edc21bb63bdc8f0b71875
ninq4 <nik.dolgov.05@mail.ru>
2024-03-09 12:33:29 +0300

    Сделал шапку сайта

---
+++ src/app/components/ui/Navbar/Navbar.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import React from 'react';
+
+
+const menuLinks = [
+  {
+    label: 'Главная',
+    url: '/',
+  },
+  {
+    label: 'О нас',
+    url: '/about',
+  },
+  {
+    label: 'Акции',
+    url: '/catalog',
+  },
+  {
+    label: 'Контакты',
+    url: '/contacts',
+  },
+];
+
+const Navbar = ({ containerStyle }: { containerStyle: string }) => {
+  return (
+    <nav className={`${containerStyle}`}>
+      <menu className="w-full flex items-center justify-center gap-4">
+        {menuLinks.map((item, pos) =>{
+            return <li key={pos}>
+                <Link className='text-white uppercase hover:text-accent transition-colors ease-linear' href={item.url}>{item.label}</Link>
+            </li>
+        })}
+      </menu>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-    
+      <Image width={100} height={30} src="./logo.svg" alt="" />
     </>
-     );
+  );
 }
